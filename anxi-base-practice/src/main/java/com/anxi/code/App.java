7ccf919256dd6ac5bc45bb2cc4d91ecfe06181fe
linJing <529189038@qq.com>
2017-09-25 14:20:37 +0800

    init

---
+++ anxi-base-practice/src/main/java/com/anxi/code/App.java
@@ -0,0 +1,13 @@
+package com.anxi.code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
